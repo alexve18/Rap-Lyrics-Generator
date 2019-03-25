8bec7361ba98fa6bafa59bc78128d6000f5881d9
nokkvikarlsson <nokkvi17@ru.is>
2019-03-25 18:16:10 +0000

    starting work on the hidden markov model

---
+++ lilMarkov.py
@@ -0,0 +1,22 @@
+import random
+import re
+
+#
+def add_to_dictionary(fileName, freqDictionary):
+    #Open the file for reading.
+    f = open(fileName, 'r')
+    #Seperate newline from words so the model doesn't associate 
+    #new line with the word in front of it.
+    words = re.sub("\n", " \n", f.read()).lower().split(' ')
+
+    
+
+#
+def next_spit():
+
+
+#
+def dj_spin_that_shit():
+
+
+if __name__ == '__main__'
