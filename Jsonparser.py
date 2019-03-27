5c659255d98794f886edeff49ab5a37641118ec1
alexve18 <alexandergud18@gmail.com>
2019-03-27 11:48:52 +0000

    Commented Jsonparser

--- Jsonparser.py
+++ Jsonparser.py
@@ -2,7 +2,25 @@ import os
 import json
 import re
     
-def parsetrimmedfile():
+def parsetrimmedfile(): #This trims the dataset for easier readable when using the hidden markov model
+    path = 'GeniusLyricsSet/'
+    f2 = open("lyrics-trimmed.txt", "w", encoding="utf-8")
+
+    for filename in os.listdir(path):
+        f = open(path + filename, "r")
+        if f.mode == "r":
+            parsed_json = json.loads(f.read())          #reads the file into a variable
+            text = str(parsed_json['songs'][0]['lyrics'])   #Gets the lyrics element of the parsed_json
+            text = re.sub("(\[.+\])", "", text)         #Removes [Verse] [Hook] etc.
+            text = re.sub("(?:\r?\n){2,}", " ", text)   #Removes extra blank lines
+            text = re.sub("(\")", "", text)             #Removes quotation marks
+            text = re.sub("(Chorus *(2x)*)", "", text)  #Removes when the lyrics says chorus
+            #text = re.sub("(\(.*\))", "", text)
+            f2.write(text)                              #Write it into lyrics.txt
+        f.close
+    f2.close
+
+def parsefile():    #This is the raw dataset combined into a single file
     path = 'GeniusLyricsSet/'
     f2 = open("lyrics.txt", "w", encoding="utf-8")
 
@@ -10,27 +28,7 @@ def parsetrimmedfile():
         f = open(path + filename, "r")
         if f.mode == "r":
             parsed_json = json.loads(f.read())
-            #print(parsed_json['songs'][0]['lyrics'])
-            text = str(parsed_json['songs'][0]['lyrics'])
-            text = re.sub("(\[.+\])", "", text)         #Removes [Verse] [Hook] etc.
-            text = re.sub("(?:\r?\n){2,}", " ", text)   #Removes extra blank lines
-            text = re.sub("(\")", "", text)             #Removes quotation marks
-            text = re.sub("(Chorus *(2x)*)", "", text)  #Removes when the lyrics says chorus
-            #text = re.sub("(\(.*\))", "", text)
-            f2.write(text)
-        f.close
-    f2.close
-
-def parsefile():
-    path = 'GeniusLyricsSet/'
-    f2 = open("Superior.txt", "w", encoding="utf-8")
-
-    for filename in os.listdir(path):
-        f = open(path + filename, "r")
-        if f.mode == "r":
-            parsed_json = json.loads(f.read())
             f2.write(str(parsed_json['songs'][0]['lyrics']))
-            #print(parsed_json['songs'][0]['lyrics'])
         f.close
     f2.close
 
